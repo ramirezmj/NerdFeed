f7f2ae5ef56539a3786483ce55ea48e379203e19
Jose Manuel Ramírez Martínez <ramirezmj1989@gmail.com>
2015-10-19 23:24:42 +0200

    ListView now loads courses from XML

--- app/src/main/java/edu/jmramirez/nerdfeed/MainActivity.java
+++ app/src/main/java/edu/jmramirez/nerdfeed/MainActivity.java
@@ -2,12 +2,23 @@ package edu.jmramirez.nerdfeed;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView coursesListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        coursesListView = (ListView) findViewById(R.id.coursesListView);
+
+        ArrayAdapter<CharSequence> coursesAdapter = ArrayAdapter
+                .createFromResource(this, R.array.courses, R.layout.course_cell);
+        coursesListView.setAdapter(coursesAdapter);
+
     }
 }
